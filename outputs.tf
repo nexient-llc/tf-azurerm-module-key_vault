5f90e0681e69a9453f79ac5f59cf85544a035e2a
Debasish Sahoo <dsahoo@nexient.com>
2022-06-10 10:08:11 -0400

    Output the key vault name

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "access_policies_object_ids" {
 output "other_access_policies" {
 	value = try(azurerm_key_vault_access_policy.access_policy)
 }
+
+output "key_vault_name" {
+	value = azurerm_key_vault.key_vault.name
+}
