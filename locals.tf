aaf551fa6e9e739137e7e0dd0027c02833efa9a2
Debasish Sahoo <dsahoo@nexient.com>
2022-06-14 09:44:01 -0400

    Deleted the build and release yamls as they are not used yet
    Added scripts for certs, secrets and keys (#11)
    
    * Added scripts for certs, secrets and keys
    
    * Formatted using terraform fmt

--- locals.tf
+++ locals.tf
@@ -1,36 +1,36 @@
 locals {
-	default_tags = {
-		"provisioner": "Terraform"
-	}
+  default_tags = {
+    "provisioner" : "Terraform"
+  }
 
-	tags = merge(local.default_tags, var.custom_tags)
+  tags = merge(local.default_tags, var.custom_tags)
 
-	default_certificate_permissions = ["List"]
+  default_certificate_permissions = ["List"]
 
-	default_key_permissions = ["List"]
+  default_key_permissions = ["List"]
 
-	default_secret_permissions = ["List"]
+  default_secret_permissions = ["List"]
 
-	default_storage_permissions = ["List"]
+  default_storage_permissions = ["List"]
 
-	default_access_policy = {
-		"default_policy"     = {
-      object_id     = data.azurerm_client_config.current.object_id
-      tenant_id     = data.azurerm_client_config.current.tenant_id
+  default_access_policy = {
+    "default_policy" = {
+      object_id = data.azurerm_client_config.current.object_id
+      tenant_id = data.azurerm_client_config.current.tenant_id
       certificate_permissions = [
-      "Get", "List", "Import", "Update", "ListIssuers", "GetIssuers", "Delete", "Recover", "Purge"
-			]
-			key_permissions = [
-				"Get", "List", "Delete", "Create"
-			]
-			secret_permissions = [
-				"Get", "List", "Delete", "Set"
-			]
-			storage_permissions = [
-				"Get", "List", "Delete", "Set"
-			]
+        "Get", "List", "Import", "Update", "ListIssuers", "GetIssuers", "Delete", "Recover", "Purge"
+      ]
+      key_permissions = [
+        "Get", "List", "Delete", "Create", "Purge"
+      ]
+      secret_permissions = [
+        "Get", "List", "Delete", "Set", "Purge"
+      ]
+      storage_permissions = [
+        "Get", "List", "Delete", "Set"
+      ]
     }
-	}
+  }
 
-	access_policies = merge(local.default_access_policy, var.access_policies)
+  access_policies = merge(local.default_access_policy, var.access_policies)
 }
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,36 @@
 output "key_vault_id" {
-	value = azurerm_key_vault.key_vault.id
+  value = azurerm_key_vault.key_vault.id
 }
 
 output "vault_uri" {
-	value = azurerm_key_vault.key_vault.vault_uri
+  value = azurerm_key_vault.key_vault.vault_uri
 }
 
 output "access_policies_object_ids" {
-	value = try(azurerm_key_vault.key_vault.access_policy[*].object_id)
+  value = try(azurerm_key_vault.key_vault.access_policy[*].object_id)
 }
 
 output "key_vault_name" {
-	value = azurerm_key_vault.key_vault.name
+  value = azurerm_key_vault.key_vault.name
+}
+
+output "certificate_ids" {
+  value = [
+    for cert in azurerm_key_vault_certificate.certs : cert.id
+  ]
+  sensitive = false
+}
+
+output "secret_ids" {
+  value = [
+    for secret in azurerm_key_vault_secret.vault_secrets : secret.id
+  ]
+  sensitive = false
+}
+
+output "key_ids" {
+  value = [
+    for key in azurerm_key_vault_key.vault_keys : key.id
+  ]
+  sensitive = false
 }
